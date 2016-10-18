492b23c8f10666c79a59279074cad2ca1b07a9b1
Kael Shipman <kael.shipman@gmail.com>
2016-10-18 00:21:44 -0500

    More updates in development

--- Exceptions.php
+++ Exceptions.php
@@ -1,6 +1,9 @@
 <?php
 namespace Skel;
 
+class ApplicationPermissionsException extends \RuntimeException {}
+class DataValidationException extends \RuntimeException {}
+class Http404Exception extends \RuntimeException {}
 class UnauthenticatedUserException extends \RuntimeException { }
 class UnauthorizedFunctionAccessException extends \RuntimeException { }
 class UndefinedActionException extends \RuntimeException {}
